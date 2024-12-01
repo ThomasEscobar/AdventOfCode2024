51dd16b09779c303e1aa4e4fd9ec8b0e1982c864
ThomasEscobar <thomas.escobarmunoz@gmail.com>
2024-12-01 23:35:24 +0000

    Using new namespace formatting

--- Logging.cs
+++ Logging.cs
@@ -1,32 +1,31 @@
 using Serilog;
 using Serilog.Core;
 
-namespace AdventOfCode.Logging
+namespace AdventOfCode.Logging;
+
+public class CustomLogging
 {
-    public class CustomLogging
+    public static Logger Init(string outputFilePath = null)
     {
-        public static Logger Init(string outputFilePath = null)
+        var loggerConfig = new LoggerConfiguration();
+
+        loggerConfig = loggerConfig.MinimumLevel.Verbose();
+
+        // Add file as a target
+        if (!string.IsNullOrEmpty(outputFilePath))
         {
-            var loggerConfig = new LoggerConfiguration();
-
-            loggerConfig = loggerConfig.MinimumLevel.Verbose();
-
-            // Add file as a target
-            if (!string.IsNullOrEmpty(outputFilePath))
+            if (File.Exists(outputFilePath))
             {
-                if (File.Exists(outputFilePath))
-                {
-                    File.Delete(outputFilePath);
-                }
-                loggerConfig.WriteTo.File(outputFilePath);
+                File.Delete(outputFilePath);
             }
-
-            // Add console as a target
-            loggerConfig.WriteTo.Console(outputTemplate: "{Level} - {Message} {Exception} {NewLine}");
-
-            var logger = loggerConfig.CreateLogger();
-            Log.Logger = logger;
-            return logger;
+            loggerConfig.WriteTo.File(outputFilePath);
         }
+
+        // Add console as a target
+        loggerConfig.WriteTo.Console(outputTemplate: "{Level} - {Message} {Exception} {NewLine}");
+
+        var logger = loggerConfig.CreateLogger();
+        Log.Logger = logger;
+        return logger;
     }
 }
--- Toolbox.cs
+++ Toolbox.cs
@@ -1,69 +1,68 @@
-namespace AdventOfCode.ToolBox
+namespace AdventOfCode.ToolBox;
+
+public static class ToolBoxClass
 {
-    public static class ToolBoxClass
+    public static List<int> GetIntListFromInput(string inputFilePath = null)
     {
-        public static List<int> GetIntListFromInput(string inputFilePath = null)
+        if (!File.Exists(inputFilePath))
         {
-            if (!File.Exists(inputFilePath))
-            {
-                throw new Exception($"Error trying to open the input file: file doesn't exist. Path: {inputFilePath}");
-            }
-
-            var list = new List<int>();
-            var inputLines = File.ReadAllLines(inputFilePath);
-
-            foreach (var line in inputLines)
-            {
-                int i;
-                if (Int32.TryParse(line, out i))
-                {
-                    list.Add(i);
-                }
-                else
-                {
-                    throw new Exception($"Error trying to parse a line from input file to int. Line content is: {line}");
-                }
-            }
-
-            return list;
+            throw new Exception($"Error trying to open the input file: file doesn't exist. Path: {inputFilePath}");
         }
 
-        public static List<double> GetDoubleListFromInput(string inputFilePath = null)
+        var list = new List<int>();
+        var inputLines = File.ReadAllLines(inputFilePath);
+
+        foreach (var line in inputLines)
         {
-            if (!File.Exists(inputFilePath))
+            int i;
+            if (Int32.TryParse(line, out i))
             {
-                throw new Exception($"Error trying to open the input file: file doesn't exist. Path: {inputFilePath}");
+                list.Add(i);
             }
-
-            var list = new List<double>();
-            var inputLines = File.ReadAllLines(inputFilePath);
-
-            foreach (var line in inputLines)
+            else
             {
-                double i;
-                if (Double.TryParse(line, out i))
-                {
-                    list.Add(i);
-                }
-                else
-                {
-                    throw new Exception($"Error trying to parse a line from input file to double. Line content is: {line}");
-                }
+                throw new Exception($"Error trying to parse a line from input file to int. Line content is: {line}");
             }
-
-            return list;
         }
 
-        public static List<string> GetStringListFromInput(string inputFilePath = null)
-        {
-            if (!File.Exists(inputFilePath))
-            {
-                throw new Exception($"Error trying to open the input file: file doesn't exist. Path: {inputFilePath}");
-            }
+        return list;
+    }
 
-            var list = new List<string>();
-            list.AddRange(File.ReadAllLines(inputFilePath));
-            return list;
+    public static List<double> GetDoubleListFromInput(string inputFilePath = null)
+    {
+        if (!File.Exists(inputFilePath))
+        {
+            throw new Exception($"Error trying to open the input file: file doesn't exist. Path: {inputFilePath}");
         }
+
+        var list = new List<double>();
+        var inputLines = File.ReadAllLines(inputFilePath);
+
+        foreach (var line in inputLines)
+        {
+            double i;
+            if (Double.TryParse(line, out i))
+            {
+                list.Add(i);
+            }
+            else
+            {
+                throw new Exception($"Error trying to parse a line from input file to double. Line content is: {line}");
+            }
+        }
+
+        return list;
+    }
+
+    public static List<string> GetStringListFromInput(string inputFilePath = null)
+    {
+        if (!File.Exists(inputFilePath))
+        {
+            throw new Exception($"Error trying to open the input file: file doesn't exist. Path: {inputFilePath}");
+        }
+
+        var list = new List<string>();
+        list.AddRange(File.ReadAllLines(inputFilePath));
+        return list;
     }
 }
